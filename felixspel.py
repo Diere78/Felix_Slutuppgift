7fb92d9921d45df1335c9a715f77ad3708a5a169
Felix Ö <felix.ortlund@elev.ga.ntig.se>
2020-04-22 09:57:44 +0200

    12

--- felixspel.py
+++ felixspel.py
@@ -1,3 +1,15 @@
 import arcade
 import random
-import 
+import math
+import os
+from typing import cast
+from the_game import Game
+
+
+def main():
+    window = Game()
+    window.start_game()
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
